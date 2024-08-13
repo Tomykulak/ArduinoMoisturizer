72db2be06b81db9cca59bca7fd9bf0619669d61b
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-08-13 23:08:33 +0200

    Added secrets to be default ignored

--- moisturizerWifiSecrets.h
+++
@@ -1,8 +0,0 @@
-// #include "moisturizerWifiSecrets.h"
-
-#define ARDUINO_IP_ADRESS "Host: 172.16.122.181"
-
-#define WIFI_SSID "your_wifi_name"
-#define WIFI_PASSWORD "your_wifi_password"
-#define API_KEY "you_api_key_in_firebase_project"
-#define DATABASE_URL "your_database_url"
