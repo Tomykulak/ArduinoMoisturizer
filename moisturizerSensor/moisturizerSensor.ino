1eed27ff96e31870bbe334f64208aca3bf2ddee9
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-08-28 13:00:04 +0200

    More readable code + try catch for error handling

--- moisturizerSensor/moisturizerSensor.ino
+++ moisturizerSensor/moisturizerSensor.ino
@@ -13,6 +13,48 @@ int moisturePercent = 0;    // Variable to store the moisture percentage
 int dry = 450;
 int wet = 300;
 
+int getMoisturePercentage(int moistureValue, int wet, int dry) {
+    int moisturePercent;
+
+    if (moistureValue >= dry) {
+        moisturePercent = 0;  // Completely dry
+    } else if (moistureValue <= wet) {
+        moisturePercent = 100;  // Completely wet
+    } else {
+        moisturePercent = map(moistureValue, wet, dry, 100, 0);
+    }
+    return moisturePercent;
+}
+
+void displayData(int moisturePercent){
+  // Display moisture percentage on the LCD
+  if(moisturePercent != -1){
+    lcd.setCursor(1, 0);
+    lcd.print("Moisture: ");
+    lcd.print(moisturePercent);
+    lcd.print("%");
+    delay(1500);
+    lcd.clear();
+
+    // Open valve if moisturePercent is below 50%
+    if (moisturePercent < 50) {
+      digitalWrite(relayPin, HIGH);  // Open the valve
+      lcd.print("Opening valve");
+    } else {
+      digitalWrite(relayPin, LOW);   // Close the valve
+      lcd.print("Valve closed");
+    }
+    delay(1000);
+    lcd.clear();
+    } else {
+      lcd.setCursor(1, 0);
+      lcd.print("BAD DATA ERROR");
+    }
+    // add delay before removing message
+    delay(1500);
+    lcd.clear();
+}
+
 void setup() {
   Serial.begin(115200); // Start Serial communication with ESP8266
   // lcd light on
@@ -23,39 +65,22 @@ void setup() {
   pinMode(relayPin, OUTPUT);
 }
 
+
 void loop() {
-  // Read values from the sensor
   moistureValue = analogRead(moisturePin);
 
-  // Map moistureValue to a percentage where:
-  if (moistureValue >= dry) {
-    moisturePercent = 0;  // Completely dry
-  } else if (moistureValue <= wet) {
-    moisturePercent = 100;  // Completely wet
+  // Check if sensor gets data
+  if (moistureValue < 0) {
+    // Handle the error value
+    Serial.println("Error: Failed to read from moisture sensor");
+    moisturePercent = -1; // Assign a default error value
   } else {
-    moisturePercent = map(moistureValue, wet, dry, 100, 0);
+    // Map moistureValue to a percentage
+    moisturePercent = getMoisturePercentage(moistureValue, wet, dry);
+    displayData(moisturePercent);
   }
-
-  // Display moisture percentage on the LCD
-  lcd.setCursor(1, 0);
-  lcd.print("Moisture: ");
-  lcd.print(moisturePercent);
-  lcd.print("%");
-  delay(1500);
-  lcd.clear();
-
-  // Open valve if moisturePercent is below 50%
-  if (moisturePercent < 50) {
-    digitalWrite(relayPin, HIGH);  // Open the valve
-    lcd.print("Opening valve");
-  } else {
-    digitalWrite(relayPin, LOW);   // Close the valve
-    lcd.print("Valve closed");
-  }
-  delay(1000);
-  lcd.clear();
 
   // Send moisture percentage to ESP8266
-  Serial.println(moisturePercent); // Send data to ESP8266
-  delay(1000);  // Send data every second
+  Serial.println(moisturePercent);
+  delay(1000);
 }
